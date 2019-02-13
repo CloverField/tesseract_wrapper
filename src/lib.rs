60a1e7beb81f794be82868bcba2b5fe73b80be78
Clover <blazelion3@gmail.com>
2019-02-13 18:47:18 -0500

    can sucessfully run a process.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,35 @@
+use std::process::Command;
+
+fn run_process_windows() {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+        .args(&["/C", "echo hello"])
+        .output()
+        .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+        .arg("-c")
+        .arg("echo hello")
+        .output()
+        .expect("failed to execute process")
+    };
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_shell() {
+        run_process_windows();
     }
 }
