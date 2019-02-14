88de7433a25f39cd86ddf53cc5812bbf485136d6
Clover <blazelion3@gmail.com>
2019-02-13 21:15:07 -0500

    Can now call tesseract.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
 use std::process::Command;
 
-fn run_process_windows() {
+fn run_tesseract(){
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
-        .args(&["/C", "echo hello"])
+        .args(&["/C", "tesseract"])
         .output()
         .expect("failed to execute process")
     } else {
         Command::new("sh")
         .arg("-c")
-        .arg("echo hello")
+        .arg("tesseract")
+        .output()
+        .expect("failed to execute process")
+    };
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
+}
+
+fn run_tesseract_with_args(args: &[&str]){
+    let mut arg_string = "".to_owned();
+    for s in args {
+        arg_string.push_str(s);
+    }
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+        .args(&["/C", ("tesseract ".to_owned() + &arg_string).as_str()])
+        .output()
+        .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+        .arg("-c")
+        .arg(("tesseract ".to_owned() + &arg_string).as_str())
         .output()
         .expect("failed to execute process")
     };
@@ -29,7 +53,13 @@ mod tests {
     }
 
     #[test]
-    fn test_shell() {
-        run_process_windows();
+    fn test_tessract() {
+        run_tesseract();
+    }
+
+    #[test]
+    fn test_run_tesseract_with_args() {
+        let arg_array = ["--list-langs"];
+        run_tesseract_with_args(&arg_array);
     }
 }
